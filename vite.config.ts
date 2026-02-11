b205e7f83c83317752a1a0a3ec037efe059578a8
Miguel Pombo <mp96dev@gmail.com>
2026-02-11 15:18:27 -0300

    fix: add shadcn UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from "path";
 
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint2";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [eslint(), react()],
+  plugins: [eslint(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
